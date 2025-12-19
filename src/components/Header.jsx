01bed43ab793c648624722492cd35aa649250827
Fabrizio <fabriziocaricato1@gmail.com>
2025-12-19 20:44:32 -0300

    Header componetizado

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header>
+      <aside>
+        <nav>
+          <ul>
+            <li><a href="#about-me">Sobre mí</a></li>
+            <li><a href="#education">Educación</a></li>
+            <li><a href="#skills">Habilidades</a></li>
+            <li><a href="#proyects">Proyectos</a></li>
+            <li><a href="#contacts">Contactos</a></li>
+          </ul>
+        </nav>
+      </aside>
+    </header>
+  );
+};
+
+export default Header;
