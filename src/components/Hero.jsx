7af71e53a0f1d13f271afc7500a35388c8e2a76a
Fabrizio <fabriziocaricato1@gmail.com>
2025-12-19 20:45:27 -0300

    Hero componetizado

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+const Hero = () => {
+  return (
+    <section id="about-me" className="short-personal-presentation">
+      <h1 className="name-title">Fabrizio Caricato</h1>
+      <div className="short-description-container">
+        <h2 className="short-description">Programación de sistemas y desarrollo web fullstack</h2>
+        <p>
+          Contactate conmigo :) {`=>`} <a className="contact-me" href="mailto:fabriziocaricato1@gmail.com">fabriziocaricato1@gmail.com</a>
+        </p>
+        <div className="social-icons">
+          <a href="mailto:fabriziocaricato1@gmail.com"><i className="fa-solid fa-envelope"></i></a>
+          <a href="https://linkedin.com/in/fabrizio" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+          <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+          <a href="https://twitter.com/tuusuario" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-x-twitter"></i></a>
+          <a href="https://instagram.com/tuusuario" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
