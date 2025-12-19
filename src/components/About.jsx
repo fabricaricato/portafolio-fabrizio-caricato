b883527e6519fb6ddc77f53fc2546e65b50ea691
Fabrizio <fabriziocaricato1@gmail.com>
2025-12-19 20:47:25 -0300

    About componetizado

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <section>
+      <h2>Sobre mí</h2>
+      <p>
+        Soy estudiante universitario de Programación de Sistemas, en camino a convertirme en desarrollador web fullstack. Tengo experiencia en Python, HTML, CSS y JavaScript, y conocimientos básicos de Java. Me apasiona crear soluciones tecnológicas funcionales y aprender continuamente para mejorar como profesional.
+      </p>
+    </section>
+  );
+};
+
+export default About;
