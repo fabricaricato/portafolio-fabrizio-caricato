d82e05bb3e528fd1cc587c828ef9bb02e5c5234c
Fabrizio <fabriziocaricato1@gmail.com>
2026-01-12 00:36:37 -0300

    rehaciendo el css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,10 @@ const Header = () => {
     <header>
       <aside>
         <nav>
+          <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGxwl4fhem6m2urbLPsROL7UHP2tjF4CowszALXe5IwDIMG7C2VdCqSyLCsNi4mrqWuKJgVE215SQCpX3TOTQSUgwhOnN_kBr2FB6omLvWww&s=10" alt="profile-pic" width={30} />
+            <p>prueba</p>
+          </div>
           <ul>
             <li><a href="#section-home">Inicio</a></li>
             <li><a href="#section-about-me">Sobre mí</a></li>
